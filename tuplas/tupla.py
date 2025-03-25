8b22516ff6530abbb06a0104b0d542744e4fa1b1
mcosta <mauricioands@hotmail.com>
2025-03-25 10:34:05 -0300

    Aulas de conjuntos

---
+++ tuplas/tupla.py
@@ -0,0 +1,21 @@
+# Tuplas são imutáveis.
+
+frutas = ("laranja", "pera", "uva",)
+
+letras = tuple("python")
+
+numeros = tuple([1, 2, 3, 4])
+
+pais = ("brasil",)
+
+frutas[0]
+frutas[-1]
+
+matriz = [
+    [1, "a", 2],
+    ["b", 3, 4],
+    [6, 5, "c"],
+]
+
+# metodos
+
